3895a5459aea819c6036651c47d1a561c105fbf5
Suvan <SUVANCHENG@ICLOUD.COM>
2018-12-31 21:38:57 +0800

    模块化代码

--- README_CN.md
+++ README_CN.md
@@ -3,13 +3,92 @@
 
 ## 概述
 
+
+
+## 重温
+
+​	当时这是写的啥呀，害的我又花了两个小时才看懂当时写的啥😂。原来是这样，将接收到string数据写入`W25Q64`Flash的前半部分；再将string数据[转换为hex数据](#stringtohex)，后写入`W25Q64`Flash的后半部分；
+### Feature
+* [x] key_up: 修改波特率；默认串口9600
+* [x] key_1 : hex显示，最多存储7页
+* [x] key_2 : string显示，最多存储7页
+
+```c
+//计算给定字符串的长度
+int my_strlen(const char *str)
+{
+	const char *p = str;
+	while (*p != '\0')
+	{
+		if (*p == ' ' && *(p + 1) == ' ' && *(p + 2) == ' ')
+		{
+			break;
+		}
+		p++;
+	}
+	return p - str;
+}
+```
+
+```c
+//初始化W25Q64
+void Check_W25Q64()
+{
+	u8 i;
+	u32 r = SIZE, secpos;
+	while (SPI_Flash_ReadID() != W25Q64) //检测不到W25Q64
+	{
+		LCD_ShowString(1, 1, 200, 16, 16, "25Q64 Check Failed!");
+		delay_ms(500);
+		LCD_ShowString(1, 1, 200, 16, 16, "Please Check!      ");
+		delay_ms(500);
+		LED0 = !LED0; //DS0闪烁
+	}
+	LCD_ShowString(1, 1, 250, 16, 12, "25Q64 Ready!                    Page:");
+
+	for (i = 0; i < 6; i++) //清楚断电前数据
+	{
+		secpos = (FLASH_SIZE / 2 - SIZE - (i * 20 * SIZE)) / 4096; //hex区
+		SPI_Flash_Erase_Sector(secpos);
+		secpos = (FLASH_SIZE - SIZE - (i * 20 * SIZE)) / 4096; //string区
+		SPI_Flash_Erase_Sector(secpos);
+	}
+}
+```
+
+### stringtohex
+
+```c
+len = my_strlen(USART_RX_BUF);
+for (i = 0; i < len * 3; i += 3)//每组3个，第1个为hex高位，第2个为hex低位，第3个为空格；
+	{
+		transfer_value[i] = (USART_RX_BUF[i / 3] / 16 + 48);   //54
+		if (\
+			(USART_RX_BUF[i / 3] > 41 && USART_RX_BUF[i / 3] < 48) || \
+			(USART_RX_BUF[i / 3] > 57 && USART_RX_BUF[i / 3] < 64) || \
+			(USART_RX_BUF[i / 3] > 73 && USART_RX_BUF[i / 3] < 80) || \
+			(USART_RX_BUF[i / 3] > 89 && USART_RX_BUF[i / 3] < 96) || \
+			(USART_RX_BUF[i / 3] > 105 && USART_RX_BUF[i / 3] < 112) || \
+			(USART_RX_BUF[i / 3] > 121 && USART_RX_BUF[i / 3] <= 127))
+				transfer_value[i + 1] = (USART_RX_BUF[i / 3] - (USART_RX_BUF[i / 3] / 16) * 16 + 55);
+		else
+			transfer_value[i + 1] = (USART_RX_BUF[i / 3] - (USART_RX_BUF[i / 3] / 16) * 16 + 48);  //52
+				
+		transfer_value[i + 2] = 32; //十进制Decimal，'32'='空格'
+	}
+```
+
+
+
+
+
+## 2017年5月13日22:45:15
+
 基于正点原子的例程修改
 
 > 实验平台：MiniSTM32F103开发板 V3版本
 > 实验目的：学习STM32 SPI接口的使用,驱动W25Q64实现SPI FLASH数据读写.
 > 实验现象：本实验通过KEY_UP按键来控制W25Q64的写入，通过另外一个按键KEY0来控制W25Q64的读取。并在LCD模块上面显示相关信息。DS0提示程序正在运行。同时，我们可以通过USMART控制读取W25QXX的ID或者整片擦除。
->
->
 
 * [x] DS0(连接在PA8) 
 * [x] 串口1(波特率:9600,PA9/PA10连接在板载USB转串口芯片CH340上面)
@@ -24,8 +103,15 @@ LCD模块在开发板的LCD接口上面，必须靠右插
 不要把1820和PA0的跳线帽跳上,否则可能导致按键"不灵".  
 W25Q64和NRF24L01/SD卡接口共用SPI1,所以他们必须分时复用,使用一个的时候,必须禁止另外两个的片选.
 
+**接下来是我的工作**
+
 Suvan：
 key_up: 修改波特率
 key_1 : hex显示，最多7页
 key_2 : string显示，最多7页
 2017年5月13日22:45:15
+
+
+
+
+
--- USER/main.c
+++ USER/main.c
@@ -7,187 +7,266 @@
 #include "spi.h"
 #include "flash.h"
 
-#define SIZE 50 
-#define FLASH_SIZE 8388608 
+#define SIZE 50
+#define FLASH_SIZE 8388608
+//ÂáΩÊï∞Â£∞Êòé
 void show_string(void);
 void show_hex(void);
 void baud_rate(void);
 void show(void);
-int my_strlen(const char *str)  
-{  
-    const char *p = str;  
-    while(*p!='\0')  
-    {
-			if(*p==' '&&*(p+1)==' '&&*(p+2)==' ')
-			{
-					break;
-			}
-			p++;  
-    }  
-    return p-str;  
-}    
+void stringtohex();
+
+//ËÆ°ÁÆóÁªôÂÆöÂ≠óÁ¨¶‰∏≤ÁöÑÈïøÂ∫¶
+int my_strlen(const char *str)
+{
+	const char *p = str;
+	while (*p != '\0')
+	{
+		if (*p == ' ' && *(p + 1) == ' ' && *(p + 2) == ' ')
+		{
+			break;
+		}
+		p++;
+	}
+	return p - str;
+}
+//ÂàùÂßãÂåñW25Q64
+void Check_W25Q64()
+{
+	u8 i;
+	u32 r = SIZE, secpos;
+	while (SPI_Flash_ReadID() != W25Q64) //Ê£ÄÊµã‰∏çÂà∞W25Q64
+	{
+		LCD_ShowString(1, 1, 200, 16, 16, "25Q64 Check Failed!");
+		delay_ms(500);
+		LCD_ShowString(1, 1, 200, 16, 16, "Please Check!      ");
+		delay_ms(500);
+		LED0 = !LED0; //DS0Èó™ÁÉÅ
+	}
+	LCD_ShowString(1, 1, 250, 16, 12, "25Q64 Ready!                    Page:");
+
+	for (i = 0; i < 6; i++) //Ê∏ÖÊ•öÊñ≠ÁîµÂâçÊï∞ÊçÆ
+	{
+		secpos = (FLASH_SIZE / 2 - SIZE - (i * 20 * SIZE)) / 4096; //hexÂå∫
+		SPI_Flash_Erase_Sector(secpos);
+		secpos = (FLASH_SIZE - SIZE - (i * 20 * SIZE)) / 4096; //stringÂå∫
+		SPI_Flash_Erase_Sector(secpos);
+	}
+}
+
+//
 int main(void)
-{ 
-	u8 t,datatemp[SIZE],transfer_value[SIZE];
+{
+	u8 t, datatemp[SIZE], transfer_value[SIZE];
 	u16 i;
-	int len=0;
-	u32 r=SIZE,My_flag=15,secpos;
+	int len = 0;
+	u32 r = SIZE, My_flag = 15;
 	delay_init();
-	uart_init(9600);	 	//¥Æø⁄≥ı ºªØŒ™9600
+	uart_init(9600); //‰∏≤Âè£ÂàùÂßãÂåñ‰∏∫9600
 	LED_Init();
- 	LCD_Init();
+	LCD_Init();
 	KEY_Init();
-	SPI_Flash_Init();  	//SPI FLASH ≥ı ºªØ 
-  LCD_Fill(0,0,240,320,BLACK);//«Â≥˝∞Î∆¡	
- 	POINT_COLOR=RED;    //…Ë÷√◊÷ÃÂŒ™∫Ï…´ 
-	while(SPI_Flash_ReadID()!=W25Q64)    //ºÏ≤‚≤ªµΩW25Q64
+	SPI_Flash_Init();				 //SPI FLASH ÂàùÂßãÂåñ
+	LCD_Fill(0, 0, 240, 320, BLACK); //Ê∏ÖÈô§ÂçäÂ±è
+	POINT_COLOR = RED;				 //ËÆæÁΩÆÂ≠ó‰Ωì‰∏∫Á∫¢Ëâ≤
+	Check_W25Q64();
+	show();
+	POINT_COLOR = GREEN; //ËÆæÁΩÆÂ≠ó‰Ωì‰∏∫ËìùËâ≤
+	while (1)
 	{
-		LCD_ShowString(1,1,200,16,16,"25Q64 Check Failed!");
-		delay_ms(500);
-		LCD_ShowString(1,1,200,16,16,"Please Check!      ");
-		delay_ms(500);
-		LED0=!LED0;//DS0…¡À∏
-		}LCD_ShowString(1,1,250,16,12,"25Q64 Ready!                    Page:");
-		for(i=0;i<6;i++)  //«Â≥˛∂œµÁ«∞ ˝æ›
+		t = KEY_Scan(0);
+		switch (t)
 		{
-			secpos=(FLASH_SIZE/2-SIZE-(i*20*SIZE))/4096;   //hex«¯
-			SPI_Flash_Erase_Sector(secpos);
-			secpos=(FLASH_SIZE-SIZE-(i*20*SIZE))/4096;     //string«¯
-			SPI_Flash_Erase_Sector(secpos);
-		}
-  show();
-	POINT_COLOR=GREEN;    //…Ë÷√◊÷ÃÂŒ™¿∂…´	
-	while(1)
-	{
-		t=KEY_Scan(0);
-		switch(t)
-		{				 
-			case KEY0_PRES:{
-				show_string();
-				LED0=!LED0;
-				break;
-			}
-			case KEY1_PRES:{
-				show_hex();
-				LED1=!LED1;
-				break;
-			}
-			case WKUP_PRES:{
-				baud_rate();
-				break;
-			}
-			default:delay_ms(10);	
-		}
-		if(USART_RX_STA&0x8000)    //Determine whether to receive data
+		case KEY0_PRES: //StringÊòæÁ§∫Â≠òÂÇ®Êï∞ÊçÆ
 		{
-			SPI_Flash_Write((u8*)USART_RX_BUF,FLASH_SIZE-r,SIZE);      //tail_flashµ⁄r∏ˆµÿ÷∑¥¶ø™ º,–¥»ÎSIZE≥§∂»µƒ ˝æ›   string
-			
-			len=my_strlen(USART_RX_BUF);    //ªÒ»°¥Æø⁄◊÷∑˚¥Æ≥§∂»
-			for(i=0;i<len*3;i+=3)
+			show_string();
+			LED0 = !LED0;
+			break;
+		}
+		case KEY1_PRES: //HEXÊòæÁ§∫Â≠òÂÇ®Êï∞ÊçÆ
+		{
+			show_hex();
+			LED1 = !LED1;
+			break;
+		}
+		case WKUP_PRES: //ÂàáÊç¢Ê≥¢ÁâπÁéá
+		{
+			baud_rate();
+			break;
+		}
+		default:
+			delay_ms(10);
+		}
+		if (USART_RX_STA & 0x8000) //Âà§Êñ≠ÊòØÂê¶Êé•Êî∂Âà∞Êï∞ÊçÆ
+		{
+			SPI_Flash_Write((u8 *)USART_RX_BUF, FLASH_SIZE - r, SIZE); //tail_flashÁ¨¨r‰∏™Âú∞ÂùÄÂ§ÑÂºÄÂßã,ÂÜôÂÖ•SIZEÈïøÂ∫¶ÁöÑÊï∞ÊçÆ   string
+			len = my_strlen(USART_RX_BUF);
+			for (i = 0; i < len * 3; i += 3)//ÊØèÁªÑ3‰∏™ÔºåÁ¨¨‰∏Ä‰∏™‰∏∫ÂçÅÂÖ≠ËøõÂà∂ÁöÑÈ´ò‰ΩçÔºåÁ¨¨‰∫å‰∏™‰∏∫ÂçÅÂÖ≠ËøõÂà∂ÁöÑ‰Ωé‰ΩçÔºåÁ¨¨‰∏â‰∏™‰∏∫Á©∫Ê†ºÔºõ
 			{
-				transfer_value[i]=(USART_RX_BUF[i/3]/16+48);
-				if((USART_RX_BUF[i/3]>41&&USART_RX_BUF[i/3]<48)
-				 ||(USART_RX_BUF[i/3]>57&&USART_RX_BUF[i/3]<64)
-				 ||(USART_RX_BUF[i/3]>73&&USART_RX_BUF[i/3]<80)
-				 ||(USART_RX_BUF[i/3]>89&&USART_RX_BUF[i/3]<96)
-				 ||(USART_RX_BUF[i/3]>105&&USART_RX_BUF[i/3]<112)
-				 ||(USART_RX_BUF[i/3]>121&&USART_RX_BUF[i/3]<=127))
-					transfer_value[i+1]=(USART_RX_BUF[i/3]-(USART_RX_BUF[i/3]/16)*16+55);
+				transfer_value[i] = (USART_RX_BUF[i / 3] / 16 + 48);   //54
+
+				if (\
+				(USART_RX_BUF[i / 3] > 41 && USART_RX_BUF[i / 3] < 48) || \
+				(USART_RX_BUF[i / 3] > 57 && USART_RX_BUF[i / 3] < 64) || \
+				(USART_RX_BUF[i / 3] > 73 && USART_RX_BUF[i / 3] < 80) || \
+				(USART_RX_BUF[i / 3] > 89 && USART_RX_BUF[i / 3] < 96) || \
+				(USART_RX_BUF[i / 3] > 105 && USART_RX_BUF[i / 3] < 112) || \
+				(USART_RX_BUF[i / 3] > 121 && USART_RX_BUF[i / 3] <= 127))
+
+				transfer_value[i + 1] = (USART_RX_BUF[i / 3] - (USART_RX_BUF[i / 3] / 16) * 16 + 55);
 				else
-					transfer_value[i+1]=(USART_RX_BUF[i/3]-(USART_RX_BUF[i/3]/16)*16+48);
-					transfer_value[i+2]=32; //ø’∏Ò
+				transfer_value[i + 1] = (USART_RX_BUF[i / 3] - (USART_RX_BUF[i / 3] / 16) * 16 + 48);  //52
+				
+				transfer_value[i + 2] = 32; //ÂçÅËøõÂà∂DecimalÔºå'32'='Á©∫Ê†º'
 			}
-			SPI_Flash_Write((u8*)transfer_value,FLASH_SIZE/2-r,SIZE);  //midd_flashµ⁄r∏ˆµÿ÷∑¥¶ø™ º,–¥»ÎSIZE≥§∂»µƒ ˝æ›   hex
-			USART_RX_STA=0;    //Update receive flag
-			for(i=0;i<USART_REC_LEN;i++)
-                USART_RX_BUF[i]=0xff;   //Ω´Ω” ‹µƒ◊÷∑˚¥Æ«Âø’
-			for(i=0;i<USART_REC_LEN;i++)
-                transfer_value[i]=' '; //Ω´◊™ªªµƒ◊÷∑˚¥Æ«Âø’
+			SPI_Flash_Write((u8 *)transfer_value, FLASH_SIZE / 2 - r, SIZE); //midd_flashÁ¨¨r‰∏™Âú∞ÂùÄÂ§ÑÂºÄÂßã,ÂÜôÂÖ•SIZEÈïøÂ∫¶ÁöÑÊï∞ÊçÆ   hex
+			USART_RX_STA = 0;												 //Update receive flag
+			for (i = 0; i < USART_REC_LEN; i++)
+				USART_RX_BUF[i] = 0xff; //Â∞ÜÊé•ÂèóÁöÑÂ≠óÁ¨¶‰∏≤Ê∏ÖÁ©∫
+			for (i = 0; i < USART_REC_LEN; i++)
+				transfer_value[i] = ' '; //Â∞ÜËΩ¨Êç¢ÁöÑÂ≠óÁ¨¶‰∏≤Ê∏ÖÁ©∫
+
+			SPI_Flash_Read(datatemp, FLASH_SIZE - r, SIZE);
+			LCD_ShowString(1, My_flag, 230, 12, 12, datatemp); //ÊòæÁ§∫ËØªÂà∞ÁöÑÂ≠óÁ¨¶‰∏≤
 			
-			SPI_Flash_Read(datatemp,FLASH_SIZE-r,SIZE);
-		  LCD_ShowString(1,My_flag,230,12,12,datatemp);    //œ‘ æ∂¡µΩµƒ◊÷∑˚¥Æ
-			My_flag+=12;
-			if(My_flag>252)
+			My_flag += 12;
+			if (My_flag > 252)
 			{
-				My_flag=15;
-				LCD_Fill(0,15,239,259,BLACK);
+				My_flag = 15;
+				LCD_Fill(0, 15, 239, 259, BLACK);
 			}
-			r+=SIZE;
+			r += SIZE;
 		}
 	}
 }
+
+
 void show_string()
 {
-	static int page=0;
+	static int page = 0;
 	u8 datatemp[SIZE];
 	u16 i;
-	u32 s=SIZE;
-	LCD_Fill(0,15,239,259,BLACK);
-	for(i=15;i<252;i+=12)
+	u32 s = SIZE;
+	LCD_Fill(0, 15, 239, 259, BLACK);
+	for (i = 15; i < 252; i += 12)
 	{
-		SPI_Flash_Read(datatemp,FLASH_SIZE-s-(page*20*SIZE),SIZE);
-		LCD_ShowxNum(215,2,page+1,3,12,0);         //œ‘ æµ±«∞“≥¬Î
-		LCD_ShowString(1,i,230,12,12,datatemp);    //œ‘ æ∂¡µΩµƒ◊÷∑˚¥Æ
-		s+=SIZE;
-		if(s>i*SIZE)s=SIZE;
+		SPI_Flash_Read(datatemp, FLASH_SIZE - s - (page * 20 * SIZE), SIZE);
+		LCD_ShowxNum(215, 2, page + 1, 3, 12, 0);	//ÊòæÁ§∫ÂΩìÂâçÈ°µÁ†Å
+		LCD_ShowString(1, i, 230, 12, 12, datatemp); //ÊòæÁ§∫ËØªÂà∞ÁöÑÂ≠óÁ¨¶‰∏≤
+		s += SIZE;
+		if (s > i * SIZE)
+			s = SIZE;
 		delay_ms(100);
-	}page++;
-	if(page>6)page=0;
+	}
+	page++;
+	if (page > 6)
+		page = 0;
 }
 void show_hex()
 {
-	static int page=0;
+	static int page = 0;
 	u8 datatemp[SIZE];
 	u16 i;
-	u32 s=SIZE;
-	LCD_Fill(0,15,239,259,BLACK);
-	for(i=15;i<252;i+=12)
+	u32 s = SIZE;
+	LCD_Fill(0, 15, 239, 259, BLACK);
+	for (i = 15; i < 252; i += 12)
 	{
-		SPI_Flash_Read(datatemp,FLASH_SIZE/2-s-(page*20*SIZE),SIZE);
-		LCD_ShowxNum(215,2,page+1,3,12,0);         //œ‘ æµ±«∞“≥¬Î
-		LCD_ShowString(1,i,230,12,12,datatemp);    //œ‘ æ∂¡µΩµƒ◊÷∑˚¥Æ
-		s+=SIZE;
-		if(s>i*SIZE)s=SIZE;
+		SPI_Flash_Read(datatemp, FLASH_SIZE / 2 - s - (page * 20 * SIZE), SIZE);
+		LCD_ShowxNum(215, 2, page + 1, 3, 12, 0);	//ÊòæÁ§∫ÂΩìÂâçÈ°µÁ†Å
+		LCD_ShowString(1, i, 230, 12, 12, datatemp); //ÊòæÁ§∫ËØªÂà∞ÁöÑÂ≠óÁ¨¶‰∏≤
+		s += SIZE;
+		if (s > i * SIZE)
+			s = SIZE;
 		delay_ms(100);
-	}page++;
-	if(page>6)page=0;
+	}
+	page++;
+	if (page > 6)
+		page = 0;
 }
 void show()
 {
-	LCD_DrawRectangle(0,1,240,260);
-	LCD_ShowString(3,262,230,12,12,"SET Baudrate:9600   (by KEY_UP)");
-	POINT_COLOR=GREEN;    //…Ë÷√◊÷ÃÂŒ™LVSE
-	LCD_ShowString(3,275,230,12,12,"CurrentMode:Maxrecord range is 7 pages");
-	LCD_ShowString(3,288,230,12,12,"KEY_0:Displays STRING from serial");
-	LCD_ShowString(3,302,230,12,12,"KEY_1:Display data in HEXadecimal form");
+	LCD_DrawRectangle(0, 1, 240, 260);
+	LCD_ShowString(3, 262, 230, 12, 12, "SET Baudrate:9600   (by KEY_UP)");
+	POINT_COLOR = GREEN; //ËÆæÁΩÆÂ≠ó‰Ωì‰∏∫LVSE
+	LCD_ShowString(3, 275, 230, 12, 12, "CurrentMode:Maxrecord range is 7 pages");
+	LCD_ShowString(3, 288, 230, 12, 12, "KEY_0:Displays STRING from serial");
+	LCD_ShowString(3, 302, 230, 12, 12, "KEY_1:Display data in HEXadecimal form");
 }
 void baud_rate()
 {
-	static int i=0;
+	static int i = 0;
 	i++;
-	switch(i)
+	switch (i)
 	{
-    case 0 :uart_init(1200);   LCD_ShowString(80,262,230,12,12,"1200   ");break;
-		case 1 :uart_init(4800);   LCD_ShowString(80,262,230,12,12,"4800   ");break;
-		case 2 :uart_init(9600);   LCD_ShowString(80,262,230,12,12,"9600   ");break;
-		case 3 :uart_init(14400);  LCD_ShowString(80,262,230,12,12,"14400  ");break;
-		case 4 :uart_init(19200);  LCD_ShowString(80,262,230,12,12,"19200  ");break;
-		case 5 :uart_init(38400);  LCD_ShowString(80,262,230,12,12,"38400  ");break;
-		case 6 :uart_init(43000);  LCD_ShowString(80,262,230,12,12,"43000  ");break;
-    case 7 :uart_init(57600);  LCD_ShowString(80,262,230,12,12,"57600  ");break;
-		case 8 :uart_init(76800);  LCD_ShowString(80,262,230,12,12,"76800  ");break;
-		case 9 :uart_init(115200); LCD_ShowString(80,262,230,12,12,"115200 ");break;
-		case 10:uart_init(128000); LCD_ShowString(80,262,230,12,12,"128000 ");break;
-		case 11:uart_init(230400); LCD_ShowString(80,262,230,12,12,"230400 ");break;
-		case 12:uart_init(256000); LCD_ShowString(80,262,230,12,12,"256000 ");break;
-    case 13:uart_init(460800); LCD_ShowString(80,262,230,12,12,"460800 ");break;
-    case 14:uart_init(921600); LCD_ShowString(80,262,230,12,12,"921600 ");break;
-    case 15:uart_init(1382400);LCD_ShowString(80,262,230,12,12,"1382400");break;
-    case 16:i=0;LCD_ShowString(80,262,230,12,12,"Max....");break;
-		default:i=0;
+	case 0:
+		uart_init(1200);
+		LCD_ShowString(80, 262, 230, 12, 12, "1200   ");
+		break;
+	case 1:
+		uart_init(4800);
+		LCD_ShowString(80, 262, 230, 12, 12, "4800   ");
+		break;
+	case 2:
+		uart_init(9600);
+		LCD_ShowString(80, 262, 230, 12, 12, "9600   ");
+		break;
+	case 3:
+		uart_init(14400);
+		LCD_ShowString(80, 262, 230, 12, 12, "14400  ");
+		break;
+	case 4:
+		uart_init(19200);
+		LCD_ShowString(80, 262, 230, 12, 12, "19200  ");
+		break;
+	case 5:
+		uart_init(38400);
+		LCD_ShowString(80, 262, 230, 12, 12, "38400  ");
+		break;
+	case 6:
+		uart_init(43000);
+		LCD_ShowString(80, 262, 230, 12, 12, "43000  ");
+		break;
+	case 7:
+		uart_init(57600);
+		LCD_ShowString(80, 262, 230, 12, 12, "57600  ");
+		break;
+	case 8:
+		uart_init(76800);
+		LCD_ShowString(80, 262, 230, 12, 12, "76800  ");
+		break;
+	case 9:
+		uart_init(115200);
+		LCD_ShowString(80, 262, 230, 12, 12, "115200 ");
+		break;
+	case 10:
+		uart_init(128000);
+		LCD_ShowString(80, 262, 230, 12, 12, "128000 ");
+		break;
+	case 11:
+		uart_init(230400);
+		LCD_ShowString(80, 262, 230, 12, 12, "230400 ");
+		break;
+	case 12:
+		uart_init(256000);
+		LCD_ShowString(80, 262, 230, 12, 12, "256000 ");
+		break;
+	case 13:
+		uart_init(460800);
+		LCD_ShowString(80, 262, 230, 12, 12, "460800 ");
+		break;
+	case 14:
+		uart_init(921600);
+		LCD_ShowString(80, 262, 230, 12, 12, "921600 ");
+		break;
+	case 15:
+		uart_init(1382400);
+		LCD_ShowString(80, 262, 230, 12, 12, "1382400");
+		break;
+	case 16:
+		i = 0;
+		LCD_ShowString(80, 262, 230, 12, 12, "Max....");
+		break;
+	default:
+		i = 0;
 	}
 }
-
-
-
-
